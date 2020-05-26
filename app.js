d66ffd35fdef97fad7fb97b3e5fc748c792fe5b1
Shadin <shadin710@gmail.com>
2020-05-26 15:17:22 +0600

    added frontend

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname +'./views')));
 
-app.set('view engine','pug');
+app.set('view engine','ejs');
 app.set('views','./views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+app.get('/',(req,res)=>{
+    res.render('index');
+});
 
 
 app.listen(port,()=>{
